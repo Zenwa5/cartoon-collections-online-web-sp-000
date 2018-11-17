2b7b3d4f3bdb6901e6de87233dfdc7ec419d6e57
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-17 04:01:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,12 @@ def summon_captain_planet(planeteer_calls)
   end
 
 
-def long_planeteer_calls(calls_longx)
-  # Your code here
+def long_planeteer_calls(calls_long)
+  if calls_long.length > 4
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
