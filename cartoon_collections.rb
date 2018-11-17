8327236288d7a64a7e910526cc8f66fea9a60781
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-17 04:06:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,4 +23,7 @@ end
 def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if cheddar_cheese.include?(cheese_types)
+    
 end
