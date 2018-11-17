f699449579ac6cc7b8e2058d5c66ce5ccaebafb4
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-17 02:28:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls)
   while planeteer_calls.length > i
     names2 = []
     planeteer_calls.map do |names|
-      names + "!"
+      names.upcase + "!"
       names2 << names
        i += 1
      end
